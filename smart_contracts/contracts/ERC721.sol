da2d4075c8746f74b01f11db2d3989683eba7abe
Deshmukh P <deshmukhpurushothaman@gmail.com>
2022-11-10 20:23:03 +0530

    ✅Basic approval concept implemented

--- smart_contracts/contracts/ERC721.sol
+++ smart_contracts/contracts/ERC721.sol
@@ -14,11 +14,18 @@ contract ERC721 {
     // Variables
     mapping(uint256 => address) private _tokenOwner; // Map tokenId to owner address
     mapping(address => uint256) private _OwnerTokenCount; // Map number of tokens owned by each user/address
+    mapping(address => uint256) private _ownedTokensCount;
+    mapping(uint256 => address) private _tokenApprovals;
 
     // Events
     event Transfer(
         address indexed from,
         address indexed to,
+        uint256 indexed tokenId
+    );
+    event Approval(
+        address indexed owner,
+        address indexed approved,
         uint256 indexed tokenId
     );
 
@@ -56,5 +63,49 @@ contract ERC721 {
         _tokenOwner[tokenId] = to; // Mapping address to tokenId
         _OwnerTokenCount[to] += 1; // Mapping number of tokens to address
         emit Transfer(address(0), to, tokenId);
+    }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal {
+        require(_to != address(0), "ERC721: Error transfer to zero address");
+        require(
+            ownerOf(_tokenId) == _from,
+            "ERC721: Trying to tranfer a token with address code"
+        );
+        _ownedTokensCount[_from] -= 1;
+        _ownedTokensCount[_from] += 1;
+        _tokenOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        _transferFrom(_from, _to, _tokenId);
+    }
+
+    function approve(address _to, uint256 _tokenId) public {
+        address owner = ownerOf(_tokenId);
+        require(_to != owner, "ERC721: Trying to approve to current owner"); // Shouldn't transfer to owner itself
+        require(msg.sender == owner, "ERC721: Unauthorized approval"); // Called by owner
+        _tokenApprovals[_tokenId] = _to;
+        emit Approval(owner, _to, _tokenId);
+    }
+
+    function isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        returns (bool)
+    {
+        require(_exists(tokenId), "ERC721: token does not exist");
+        address owner = ownerOf(tokenId);
+        require(spender == owner, "ERC721: Unauthorized approval"); // Called by owner
+        // Write getApproved function on own similar to openzeppelin
     }
 }
